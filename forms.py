7457b74261a7b0eddd6bf628120878a13038824d
mirimi <magasomarymae@gmail.com>
2026-05-07 19:16:56 +0800

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,13 +4,16 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=150,
+        strip=True,
         widget=forms.TextInput(attrs={
             'placeholder': 'enter username',
             'class': 'form-input',
             'autocomplete': 'username',
         })
     )
+
     password = forms.CharField(
+        required=True,
         widget=forms.PasswordInput(attrs={
             'placeholder': 'enter password',
             'class': 'form-input',
